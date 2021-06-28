0b78208327be7455708ac1c3827a85c93f430188
Ronit Pradhan <ronpradh@cisco.com>
2021-06-28 19:35:39 +0530

    Adding Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import { AnimatePresence } from 'framer-motion';
 
+import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
+import useGaTracker from './GATracker';
+
 function App() {
+
+  //Google Analytics 
+  useGaTracker();
+
   return (
     <>
-      <Router basename="/portfolio-website">
-        <ScrollToTop>
-          <AnimatePresence exitBeforeEnter>
-            <Switch>
-              <Route path='/' exact component={Home} />
-              <Route path='/contact' exact component={Contact} />
-              {/* Redirect other paths to home */}
-              <Redirect to='/' />
-            </Switch>
-          </AnimatePresence>
-        </ScrollToTop>
-      </Router>
+      <ScrollToTop>
+        <AnimatePresence exitBeforeEnter>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/contact' exact component={Contact} />
+            {/* Redirect other paths to home */}
+            <Redirect to='/' />
+          </Switch>
+        </AnimatePresence>
+      </ScrollToTop>
     </>
   );
 }
